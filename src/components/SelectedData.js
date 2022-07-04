b469190839f5481468d1a1ef05c81cc61552106b
Sergey Novozhylov <sergey.novozhylov@gmail.com>
2022-07-04 10:35:26 +0100

    fixes

--- src/components/SelectedData.js
+++ src/components/SelectedData.js
@@ -8,26 +8,27 @@ const SeletedData = ({ clickHadler=()=>{}, searchField=false }) => {
 console.log(searchField);
     return (
         <>
-            { !_.isEmpty(calculateData.insuranceProvider) && (
+            { (!_.isEmpty(calculateData.insuranceProvider) || searchField) && (
                 <Grid item onClick={()=> clickHadler('insurance')}>
                     <Typography variant='h5'>Provider</Typography>
                     <Typography variant='h3' sx={{ color: (searchField && searchField!=='insurance') ? '#888484' : '#161616' }}>{calculateData?.insuranceProvider.name}</Typography>
                 </Grid> 
             )}
-            { !_.isEmpty(calculateData.insurancePlan) && (
+            { (!_.isEmpty(calculateData.insurancePlan) || searchField) && (
                 <Grid item mt="24px" onClick={()=> clickHadler('insurance')}>
                     <Typography variant='h5'>Plan</Typography>
                     <Typography variant='h3' sx={{ color: (searchField && searchField!=='insurance') ? '#888484' : '#161616' }}>{calculateData?.insurancePlan.name}</Typography>
                 </Grid>
             )}
-            { !_.isEmpty(calculateData.procedure) && (
+            { (!_.isEmpty(calculateData.procedure) || searchField) && (
             <Grid item mt="24px" onClick={()=> clickHadler('procedure')}>
                 <Typography variant='h5'>Procedure</Typography>
                 <Typography variant='h3' sx={{ color: (searchField && searchField!=='procedure') ? '#888484' : '#161616' }}>{calculateData?.procedure.billing_code}:{calculateData?.procedure.name}</Typography>
             </Grid>
             )}
-            { ( (calculateData.location.zipCode && calculateData.location.locationType==='zipcode') || 
-                    (calculateData.location.lat && calculateData.location.locationType==='location') ) && (
+            { ( ((calculateData.location.zipCode && calculateData.location.locationType==='zipcode') || 
+                    (calculateData.location.lat && calculateData.location.locationType==='location') ) ||
+                    searchField ) && (
                 <Grid item mt="24px" onClick={()=> clickHadler('location')}>
                     <Typography variant='h5'>Location</Typography>
                     <Typography variant='h3' sx={{ color: (searchField && searchField!=='location') ? '#888484' : '#161616' }}>
